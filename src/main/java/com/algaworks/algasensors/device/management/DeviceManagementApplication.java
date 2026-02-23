bf4e8ec16a6c9f0ca3e03986b4de33aa3260fffe
Francisco Sousa <franciscoeds1987@gmail.com>
2026-02-23 18:09:22 -0300

    Adicionando plugins do Gradle

--- src/main/java/com/algaworks/algasensors/device/management/DeviceManagementApplication.java
+++ src/main/java/com/algaworks/algasensors/device/management/DeviceManagementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeviceManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeviceManagementApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(DeviceManagementApplication.class, args);
+    }
 
 }
