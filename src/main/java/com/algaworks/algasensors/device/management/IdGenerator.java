410ee54e15739c7cc711a1de39d57fa6de550974
Francisco Sousa <franciscoeds1987@gmail.com>
2026-02-24 18:56:20 -0300

    Usando o TSID com Java

---
+++ src/main/java/com/algaworks/algasensors/device/management/IdGenerator.java
@@ -0,0 +1,36 @@
+package com.algaworks.algasensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+
+import java.util.Optional;
+
+public class IdGenerator {
+
+    private static final String TSID_NODE_COUNT = "tsid.node.count";
+    private static final String TSID_NODE = "tsid.node";
+    private static final String DEFAULT_NODE_COUNT = "32";
+
+    private static final TSID.Factory tsidFactory;
+
+    static {
+        Optional.ofNullable(System.getenv(TSID_NODE))
+                .ifPresent(tsidNode -> System.setProperty(TSID_NODE, tsidNode));
+
+        Optional.ofNullable(System.getenv(TSID_NODE_COUNT))
+                .ifPresent(tsidNodeCount -> System.setProperty(TSID_NODE_COUNT, tsidNodeCount));
+
+
+        System.setProperty(TSID_NODE_COUNT, DEFAULT_NODE_COUNT);
+
+        tsidFactory = TSID.Factory.builder().build();
+    }
+
+    private IdGenerator() {
+
+    }
+
+    public static TSID generateTSID() {
+        return tsidFactory.generate();
+    }
+
+}
