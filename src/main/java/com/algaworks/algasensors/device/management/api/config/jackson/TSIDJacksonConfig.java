49f1faa18375bdb97e477f36a31e7997080e4ae6
Francisco Sousa <franciscoeds1987@gmail.com>
2026-02-25 17:28:22 -0300

    Implementando um serializador customizado para TSID com Jackson

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/config/jackson/TSIDJacksonConfig.java
@@ -0,0 +1,19 @@
+package com.algaworks.algasensors.device.management.api.config.jackson;
+
+import io.hypersistence.tsid.TSID;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.JacksonModule;
+import tools.jackson.databind.module.SimpleModule;
+
+@Configuration
+public class TSIDJacksonConfig {
+
+    @Bean
+    public JacksonModule tsidModule() {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(TSID.class, new TSIDToStringSerializer());
+        return module;
+    }
+
+}
